6c2ea40a9c19a849e801df918f86aceadd7fa7fc
Evan Poirier <1evanpoirier4@gmail.com>
2026-04-15 15:28:05 -0500

    remove unnecessary files

--- submission/myscript.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source /apps/profiles/modules_asax.sh.dyn
-module load cuda
-
-./gpu-life 5120 5000 /scratch/$USER/output.5120.5000.gpu
-./gpu-life 5120 5000 /scratch/$USER/output.5120.5000.gpu
-./gpu-life 5120 5000 /scratch/$USER/output.5120.5000.gpu
--- testscript.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source /apps/profiles/modules_asax.sh.dyn
-module load cuda
-
-./gpu-life 100 100 /scratch/$USER/output.100.100.gpu
-./life 100 100 /scratch/$USER/output.100.100.cpu
