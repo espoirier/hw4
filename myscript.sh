ef4e53dd32ce01021ffe60cd4b4f84c4a4e57b07
Evan Poirier <espoirier@crimson.ua.edu>
2026-04-15 14:02:11 -0500

    ,

---
+++ myscript.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source /apps/profiles/modules_asax.sh.dyn
+module load cuda
+
+./gpu-life 5120 5000 /scratch/$USER/output.5120.5000.gpu
+./gpu-life 5120 5000 /scratch/$USER/output.5120.5000.gpu
+./gpu-life 5120 5000 /scratch/$USER/output.5120.5000.gpu
+./life 5120 5000 /scratch/$USER/output.5120.5000.cpu
